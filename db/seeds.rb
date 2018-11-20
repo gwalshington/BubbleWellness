d088605638fbbf1a4dc59c3ee98a119ee87e3343
gwalshington <walshcostigan@gmail.com>
2018-11-20 16:14:44 -0500

    seeds for staging

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#User.create!(email: 'mia@bubblewellness.com', first_name: 'Mia', admin: true, password: 'password', password_confirmation: 'password')
-#User.create!(email: 'therapist@gmail.com', first_name: 'Sally', admin: false, therapist: true, password: 'password', password_confirmation: 'password')
-#User.create!(email: 'user@gmail.com', first_name: 'Joe', admin: false, password: 'password', password_confirmation: 'password')
-# Gender.create(name: 'cis woman')
-# Gender.create(name: 'cis man')
-# Gender.create(name: 'trans woman')
-# Gender.create(name: 'trans man')
-# Gender.create(name: 'non-binary')
-# Gender.create(name: 'other')
-#
-# Race.create(name: 'black')
-# Race.create(name: 'asian')
-# Race.create(name: 'hispanic')
+User.create!(email: 'mia@bubblewellness.com', first_name: 'Mia', admin: true, password: 'password', password_confirmation: 'password')
+User.create!(email: 'therapist@gmail.com', first_name: 'Sally', admin: false, therapist: true, password: 'password', password_confirmation: 'password')
+User.create!(email: 'user@gmail.com', first_name: 'Joe', admin: false, password: 'password', password_confirmation: 'password')
+
+Gender.create(name: 'cis woman')
+Gender.create(name: 'cis man')
+Gender.create(name: 'trans woman')
+Gender.create(name: 'trans man')
+Gender.create(name: 'non-binary')
+Gender.create(name: 'other')
+
+Race.create(name: 'black')
+Race.create(name: 'asian')
+Race.create(name: 'hispanic')
 
 TherapyFormat.create(name: 'In Person')
 TherapyFormat.create(name: 'Virtual')
